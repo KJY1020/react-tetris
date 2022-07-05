3900447be8a0aaaed3848f0cb75c98932e644e03
KJY1020 <seawater1020@gmail.com>
2022-07-05 15:11:47 +0900

    Updates

---
+++ precache-manifest.bed07b8143a64830735793c2b345b797.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e65bd47c8714de8f3ae9488433914488",
+    "url": "/react-tetris/index.html"
+  },
+  {
+    "revision": "b8679bec0325d18e1a83",
+    "url": "/react-tetris/static/css/main.13370a51.chunk.css"
+  },
+  {
+    "revision": "98bffe7b3ff03b4bfe5d",
+    "url": "/react-tetris/static/js/2.136719ee.chunk.js"
+  },
+  {
+    "revision": "b8679bec0325d18e1a83",
+    "url": "/react-tetris/static/js/main.08d31c9f.chunk.js"
+  },
+  {
+    "revision": "029efe1d95d0c6250d6f",
+    "url": "/react-tetris/static/js/runtime-main.b4b75ff2.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/react-tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "ae8d607158918c6527c158c8aadc9fbb",
+    "url": "/react-tetris/static/media/bg.ae8d6071.png"
+  }
+]);
